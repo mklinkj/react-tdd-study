4de0216a898bb6e7836116abbb6ed9ef81c9bdec
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-13 20:54:39 +0900

    p160. 기본 버튼 테스트 코드 작성

--- chap06/todo-list/src/App.test.tsx
+++ chap06/todo-list/src/App.test.tsx
@@ -3,31 +3,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
-  it('renders component correctly', () => {
-    const { container } = render(<App />); // 리액트 컴포넌트를 화면에 표시
-    // screen: 리액트 컴포넌트가 표시된 화면을 의미
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-
-    const appLogo = screen.getByAltText('logo');
-    expect(appLogo).toBeInTheDocument();
-    expect(appLogo).toHaveAttribute('src', 'logo.svg');
-    expect(appLogo).toHaveAttribute('alt', 'logo');
-
-    const pElement1 = screen.getByText(/Edit /i);
-    expect(pElement1).toBeInTheDocument();
-    const pElement2 = screen.getByText(/src\/App.tsx/i);
-    expect(pElement2).toBeInTheDocument();
-    const pElement3 = screen.getByText(/ and save to reload./i);
-    expect(pElement3).toBeInTheDocument();
-
-    // 테스트는 성공하지만 직접 노드에 접근하지 말고,
-    // 테스트 라이브러리 메서드 활용하라고 lint 경고 밑줄 설정됨 ㅠㅠ
-    // expect(container.getElementsByTagName('p')).toHaveLength(1);
-    // expect(container.getElementsByTagName('p')[0]).toHaveTextContent(
-    //   'Edit src/App.js and save to reload.',
-    // );
-
-    expect(container).toMatchSnapshot();
-  });
+  it('renders component correctly', () => {});
 });
---
+++ chap06/todo-list/src/Components/Button/index.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import 'jest-styled-components';
+
+import { Button } from './index';
+
+describe('<Button />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<Button label="Button Test" />); // 버튼 랜더링
+    const label = screen.getByText('Button Test'); // 랜더링된 버튼 컴포넌트를 찾음. (엄밀하게 말해서 Button 컴포넌트속 Label div를 찾는 것은 아닌지?)
+    expect(label).toBeInTheDocument(); // 화면에 표시되어있는지 확인
+    const parent = label.parentElement;
+    expect(parent).toHaveStyleRule('background-color', '#304FFE'); // jest-styled-components을 통해 toHaveStyleRule를 사용할 수 있음.
+    expect(parent).toHaveStyleRule('background-color', '#1E40FF', {
+      modifier: ':hover',
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
