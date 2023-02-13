b19f0ebef2bea89ca9a902ed92aa194e631f4ef3
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-13 22:00:04 +0900

    Input 컴포넌트 데이터 입력 테스트

--- chap06/todo-list/src/Components/Input/index.test.tsx
+++ chap06/todo-list/src/Components/Input/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import 'jest-styled-components';
 
 import { Input } from './index';
@@ -20,5 +20,17 @@ describe('<Button />', () => {
 
     const input = screen.getByPlaceholderText('default placeholder');
     expect(input).toBeInTheDocument();
+  });
+
+  // Input 컴포넌트에 키보드 입력 테스트
+  it('change the data', () => {
+    render(<Input placeholder="default placeholder" />);
+
+    // 찾은 컴포넌트는 기본적으로 HTMLElement 임, HTMLInputElement 으로 형변환
+    const input = screen.getByPlaceholderText('default placeholder') as HTMLInputElement;
+
+    fireEvent.change(input, { target: { value: 'study react' } }); // 찾은 Input 컴포넌트에 데이터 입력
+
+    expect(input.value).toBe('study react');
   });
 });
