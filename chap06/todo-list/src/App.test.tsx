a8877b21388f7ee68f17d40844fff0ec39794f4d
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-13 22:58:56 +0900

    App 컴포넌트 통합테스트
    npm run test -- --coverage 를 실행시...
    
    조건 브랜치 테스트 커버리지 하나를 완료하지 못한 상태이긴함..

--- chap06/todo-list/src/App.test.tsx
+++ chap06/todo-list/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import 'jest-styled-components';
 import exp from 'constants';
@@ -18,5 +18,35 @@ describe('<App />', () => {
     expect(label).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('add and deletes ToDo items', () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+    const button = screen.getByText('추가');
+    fireEvent.change(input, { target: { value: 'study react 1' } });
+    fireEvent.click(button); // To do 1 등록
+
+    const todoItem = screen.getByText('study react 1');
+    expect(todoItem).toBeInTheDocument();
+    const deleteButton = screen.getByText('삭제');
+    expect(deleteButton).toBeInTheDocument();
+
+    const toDoList = screen.getByTestId('toDoList');
+    expect(toDoList.childElementCount).toBe(1); // 등록을 했으니 1개가 되야함.
+
+    fireEvent.change(input, { target: { value: 'study react 2' } });
+    fireEvent.click(button); // To do 2 등록
+
+    const todoItem2 = screen.getByText('study react 2');
+    expect(todoItem2).toBeInTheDocument();
+    expect(toDoList.childElementCount).toBe(2);
+
+    const deleteButtons = screen.getAllByText('삭제');
+    fireEvent.click(deleteButtons[0]);
+
+    expect(todoItem).not.toBeInTheDocument();
+    expect(toDoList.childElementCount).toBe(1);
   });
 });
