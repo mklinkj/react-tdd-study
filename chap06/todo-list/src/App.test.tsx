7377685a2cec327f3ebe3d1b619c410f6ac96d6f
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-13 22:44:17 +0900

    App 컴포넌트 기본 테스트

--- chap06/todo-list/src/App.test.tsx
+++ chap06/todo-list/src/App.test.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import 'jest-styled-components';
+import exp from 'constants';
 
 describe('<App />', () => {
-  it('renders component correctly', () => {});
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+
+    const toDoList = screen.getByTestId('toDoList'); // 해당 컴포넌트의 data-testid 속성 값
+    expect(toDoList).toBeInTheDocument();
+    expect(toDoList.firstChild).toBeNull();
+
+    const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+    expect(input).toBeInTheDocument();
+    const label = screen.getByText('추가');
+    expect(label).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
 });
