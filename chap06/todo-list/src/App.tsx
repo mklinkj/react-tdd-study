dfdfeeaadd1671aecc3c8ac2b83a7490c2b30c82
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-13 16:31:57 +0900

    p156 테스트 전까지 진행 완료
    그냥 toDo리스트가 완성이 되었음...

--- chap06/todo-list/src/App.tsx
+++ chap06/todo-list/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styled from 'styled-components';
 import { Button, Input, ToDoItem } from 'Components';
 
@@ -23,14 +23,46 @@ const InputContainer = Styled.div`
   display: flex;
 `;
 
+const ToDoListContainer = Styled.div`
+  min-width: 350px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 1px solid #BDBDBD;
+  margin-bottom: 20px;
+`;
+
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState<string[]>([]); // TypeScript에서 빈배열은 타입추론을 할 수 없으므로 제네릭 구문 추가
+
+  const addToDo = (): void => {
+    if (toDo) {
+      setToDoList([...toDoList, toDo]);
+      setToDo('');
+    }
+  };
+
+  const deleteToDo = (index: number): void => {
+    let list = [...toDoList]; // 직접삭제할 수 없기 때문에 전개 구문으로 배열 사본만듦.
+    list.splice(index, 1);
+    setToDoList(list);
+  };
+
   return (
     <Container>
       <Contents>
-        <ToDoItem label="추가된 할일" onDelete={() => alert('삭제')} />
+        <ToDoListContainer>
+          {toDoList.map((item, index) => (
+            <ToDoItem key={item} label={item} onDelete={() => deleteToDo(index)} />
+          ))}
+        </ToDoListContainer>
         <InputContainer>
-          <Input placeholder="할 일을 입력해 주세요" onChange={(text) => console.log(text)} />
-          <Button label="추가" onClick={() => alert('추가')} />
+          <Input
+            placeholder="할 일을 입력해 주세요"
+            value={toDo}
+            onChange={(text) => setToDo(text)}
+          />
+          <Button label="추가" onClick={addToDo} />
         </InputContainer>
       </Contents>
     </Container>
