eedd8f65ac086014f4015ea774212333062d9a59
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-02-14 14:01:26 +0900

    App컴포넌트 클래스 방식으로 변경

--- chap07/class-todo-list/src/App.tsx
+++ chap07/class-todo-list/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Styled from 'styled-components';
 import { Button, Input, ToDoItem } from 'Components';
 
@@ -31,42 +31,66 @@ const ToDoListContainer = Styled.div`
   margin-bottom: 20px;
 `;
 
-function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDoList, setToDoList] = useState<string[]>([]); // TypeScript에서 빈배열은 타입추론을 할 수 없으므로 제네릭 구문 추가
+interface Props {}
 
-  const addToDo = (): void => {
+interface State {
+  readonly toDo: string;
+  readonly toDoList: string[];
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props); // 부모로 부터 전달받은 Props를 super 함수를 호출하여 상속받은 부모클래스에 반드시 전달해야함.
+
+    // 생성자 안에서 state 값 초기화
+    this.state = {
+      toDo: '',
+      toDoList: [],
+    };
+  }
+
+  private addToDo = (): void => {
+    const { toDo, toDoList } = this.state;
     if (toDo) {
-      setToDoList([...toDoList, toDo]);
-      setToDo('');
+      // 클래스 방식에서는 State값 설정을 위해 setState만 사용가능.
+      this.setState({
+        toDo: '',
+        toDoList: [...toDoList, toDo],
+      });
     }
   };
 
-  const deleteToDo = (index: number): void => {
-    let list = [...toDoList]; // 직접삭제할 수 없기 때문에 전개 구문으로 배열 사본만듦.
-    list.splice(index, 1);
-    setToDoList(list);
+  private deleteToDo = (index: number): void => {
+    const list = [...this.state.toDoList];
+    list.splice(index, 1); // 사본생성 함수라서 list가 const가 되도 될 것 같음.
+    this.setState({
+      toDoList: list,
+    });
   };
 
-  return (
-    <Container>
-      <Contents>
-        <ToDoListContainer data-testid="toDoList">
-          {toDoList.map((item, index) => (
-            <ToDoItem key={item} label={item} onDelete={() => deleteToDo(index)} />
-          ))}
-        </ToDoListContainer>
-        <InputContainer>
-          <Input
-            placeholder="할 일을 입력해 주세요"
-            value={toDo}
-            onChange={(text) => setToDo(text)}
-          />
-          <Button label="추가" onClick={addToDo} />
-        </InputContainer>
-      </Contents>
-    </Container>
-  );
+  render() {
+    const { toDo, toDoList } = this.state;
+
+    return (
+      <Container>
+        <Contents>
+          <ToDoListContainer data-testid="toDoList">
+            {toDoList.map((item, index) => (
+              <ToDoItem key={item} label={item} onDelete={() => this.deleteToDo(index)} /> // 클래스의 맴버 함수들을 this.함수명으로 호출
+            ))}
+          </ToDoListContainer>
+          <InputContainer>
+            <Input
+              placeholder="할 일을 입력해 주세요"
+              value={toDo}
+              onChange={(text) => this.setState({ toDo: text })}
+            />
+            <Button label="추가" onClick={this.addToDo} />
+          </InputContainer>
+        </Contents>
+      </Container>
+    );
+  }
 }
 
 export default App;
